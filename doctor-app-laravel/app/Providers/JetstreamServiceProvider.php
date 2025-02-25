3eda6b3e4db5bb6f1565cd9c30733c33f89524cb
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-02-25 23:38:56 +0700

    Feat: Laravel Setup - install Laravel, set database - composer require Laravel/Jetstream - php artisan Jetstream:install livewire Fortify - Setup Login User Only Doctor can login - make profile picture fitur and biodata also experience, category and api token

---
+++ doctor-app-laravel/app/Providers/JetstreamServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Jetstream\DeleteUser;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+use Laravel\Jetstream\Jetstream;
+
+class JetstreamServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // cek hanya user dan email dengan type doctor yang hanya bisa login
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+            if ($user && Hash::check($request->getPassword, $user->password) && $user->type == 'doctor') {
+                return $user;
+            }
+        });
+        $this->configurePermissions();
+
+        Jetstream::deleteUsersUsing(DeleteUser::class);
+
+        Vite::prefetch(concurrency: 3);
+    }
+
+    /**
+     * Configure the permissions that are available within the application.
+     */
+    protected function configurePermissions(): void
+    {
+        Jetstream::defaultApiTokenPermissions(['read']);
+
+        Jetstream::permissions([
+            'create',
+            'read',
+            'update',
+            'delete',
+        ]);
+    }
+}
